8b252ea64983fd9149a0b6415f3b3594cde86672
houssamlambara <lambarahoussam@gmail.com>
2024-11-12 16:19:58 +0100

    add responsive

--- src/js/Catalogue.js
+++ src/js/Catalogue.js
@@ -16,9 +16,9 @@ function displayData(data) {
                             <a href='/src/Detail.html?id=${product.id}'><h3 class="font-bold text-md mb-2" style='height: 50px; display: flex; align-items: center;'>${product.name}</h3></a>
                             <p class="text-gray-600 mb-4 text-center text-sm" style='height: 80px;'>${product.description.substring(0, 90)}... </p>
                             <p class="font-bold text-xl mb-4" style="color: #FC6736;">$${product.price}</p>
-                            <div class="flex justify-center space-x-4">
-                                <button style="background-color: #FC6736;" class="text-white px-2 py rounded-full">Add to Cart</button>
-                                 <a href='/src/Detail.html?id=${product.id}'><button style="background-color: #FC6736;" class="text-white px-2 py rounded-full">Customize</button></a>
+                            <div class="flex justify-center space-x-1">
+                                <button style="background-color: #FC6736;" class="text-white lg:text-lg px-1 text-xs rounded-full"><pre>Add to cart</pre></button>
+                                 <a href='/src/Detail.html?id=${product.id}'><button style="background-color: #FC6736;" class="text-white px-1 lg:text-lg py-1 text-xs rounded-full">Customize</button></a>
                             </div>
                         </div>
                     </div>`;
@@ -40,4 +40,19 @@ document.querySelectorAll('.filter-btn').forEach
 function filterCategorie(categorie) {
     let filteredCat = carts.filter(element => element.categorie === categorie);
     displayData(filteredCat);
+}
+
+function showPage(pageNumber) {
+    // On récupère les deux pages
+    const page1 = document.getElementById('1');
+    const page2 = document.getElementById('2');
+
+    // On montre ou cache chaque page selon le numéro choisi
+    if (pageNumber === 1) {
+        page1.classList.remove('hidden');
+        page2.classList.add('hidden');
+    } else {
+        page1.classList.add('hidden');
+        page2.classList.remove('hidden');
+    }
 }
