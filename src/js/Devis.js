37811ad3003cd3e159c75a3be454ea7e7723cc81
HamzaBraik01 <hamzabraik02@gmail.com>
2024-11-13 15:31:49 +0100

    pull the code from the main branch

---
+++ src/js/Devis.js
@@ -0,0 +1,23 @@
+import carts from "./Data.js";
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+if (day < 10) {
+    day = '0' + day;
+}
+if (month < 10) {
+    month = '0' + month;
+}
+var D = year + '-' + month + '-' + day;
+document.getElementById('today').textContent = D;
+function generateNumber() {
+    var number = "";
+    for (var i = 0; i < 4; i++) {
+        var N = Math.floor(Math.random() * 10); 
+        number += N;
+    }
+    return number;
+}
+var devisNumber = year + "-" + month + "-" + generateNumber();
+document.getElementById("devisNumber").textContent = devisNumber;
