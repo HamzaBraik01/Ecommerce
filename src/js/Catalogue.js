1238b9afd5aaf2b331fdbb517a472a14e1982474
houssamlambara <lambarahoussam@gmail.com>
2024-11-13 14:48:58 +0100

    update

--- src/js/Catalogue.js
+++ src/js/Catalogue.js
@@ -24,7 +24,7 @@ function displayData(data, paginate) {
                         <div class="flex flex-col items-center mb-2">
                             <img src="${product.image}" alt="Product Image" class="w-full h-40 object-cover rounded mb-4">
                             <a href='/src/Detail.html?id=${product.id}'><h3 class="font-bold text-md mb-2" style='height: 50px; display: flex; align-items: center;'>${product.name}</h3></a>
-                            <p class="text-gray-600 mb-4 text-center text-sm" style='height: 80px;'>${product.description.substring(0, 90)}... </p>
+                            <!--<p class="text-gray-600 mb-4 text-center text-sm" style='height: 80px;'>${product.description.substring(0, 90)}.0.. </p>-->
                             <p class="font-bold text-xl mb-4 mt-6" style="color: #FC6736;">$${product.price}</p>
                             <div class="flex justify-center space-x-4">
                                 <button style="background-color: #FC6736;" class="text-white lg:text-lg px-1 text-xs rounded-full"><pre>Add to cart</pre></button>
@@ -48,7 +48,7 @@ document.querySelectorAll('.filter-btn').forEach
     });
 
 function filterCategorie(categorie) {
-    let filteredCat = carts.filter(element => element.categorie === categorie);
+    let filteredCat = carts.filter(element => element.categorie == categorie);
     displayData(filteredCat);
 }
 
