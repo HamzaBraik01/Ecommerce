e68c87815db656169c1dcce556f8063ae739d602
HamzaBraik01 <hamzabraik02@gmail.com>
2024-11-15 15:42:40 +0100

    add function Checkout

--- src/js/Devis.js
+++ src/js/Devis.js
@@ -74,6 +74,53 @@ const total = subTotal + tax;
 document.getElementById("subtotal").textContent = `$${subTotal}`;
 document.getElementById("tax").textContent = `$${tax.toFixed(3)}`;
 document.getElementById("total").textContent = `$${total.toFixed(3)}`;
-/*document.getElementById("confirm").addEventListener("click",function(){
+//function for checkout
+function CheckoutF() {
+    const cart = (JSON.parse(localStorage.getItem("valide")) || []).flat();
+
+    if (cart.length == 0) {
+        alert("Votre panier est vide !");
+        return;
+    }
     
-})*/
+    const confirmationTableBody = document.getElementById("confirmationTableBody");
+    confirmationTableBody.innerHTML = ""; 
+
+    let total1 = 0;
+    //const cartLength = cart.length;
+
+    confirmationTableBody.innerHTML = "";
+
+    for (let i = 0; i < cart.length; i++) {
+        const itemTotal = cart[i].price * cart[i].quantity;
+        total1 += itemTotal;
+
+        const row = `
+            <tr class="${i % 2 == 0 ? 'bg-red-100' : 'bg-white'} text-center">
+                <td class="py-2 px-2 border">${i + 1}</td>
+                <td class="py-2 px-2 border">${cart[i].titre}</td>
+                <td class="py-2 px-2 border">$${cart[i].price}</td>
+                <td class="py-2 px-2 border">${cart[i].quantity}</td>
+                <td class="py-2 px-2 border">$${itemTotal.toFixed(2)}</td>
+            </tr>
+        `;
+        confirmationTableBody.innerHTML += row;
+    }
+
+   
+    document.getElementById("confirmationTotal").textContent = `$${total.toFixed(2)}`;
+    document.getElementById("confirmationMessage").classList.remove("hidden"); 
+}
+
+document.getElementById("closeConfirmation").addEventListener("click", () => {
+    document.getElementById("confirmationMessage").classList.add("hidden");
+});
+
+document.getElementById("payNow").addEventListener("click", () => {
+    localStorage.removeItem("valide");
+    document.getElementById("confirmationMessage").classList.add("hidden");
+    window.location.href = "Home.html";
+});
+
+document.getElementById("Checkout").addEventListener("click", CheckoutF);
+
