fdd322c9f0461fb9e68ed0f36d43df515574d9c6
HamzaBraik01 <hamzabraik02@gmail.com>
2024-11-15 11:49:38 +0100

    add cart in table devis

--- src/js/Devis.js
+++ src/js/Devis.js
@@ -1,3 +1,79 @@
 
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
 
-console.log(JSON.parse(localStorage.getItem("valide")))
+if (day < 10) {
+  day = '0' + day;
+}
+if (month < 10) {
+  month = '0' + month;
+}
+
+var D = year + '-' + month + '-' + day;
+document.getElementById('today').textContent = D;
+
+function generateNumber() {
+  let number = "";
+  for (let i = 0; i < 4; i++) {
+    let N = Math.floor(Math.random() * 10); 
+    number += N;
+  }
+  return number;
+}
+
+let devisNumber = localStorage.getItem("devisNumber");
+
+if (!devisNumber) {
+  devisNumber = year + "-" + month + "-" + generateNumber();
+  localStorage.setItem("devisNumber", devisNumber);
+}
+
+document.getElementById("devisNumber").textContent = devisNumber;
+
+
+function exitToHome() {
+  window.location.href = "Home.html"; 
+}
+document.getElementById("exitButton").addEventListener("click", exitToHome);
+
+const cart = (JSON.parse(localStorage.getItem("valide")) || []).flat();
+/*console.log(cart)*/
+const tableBody = document.getElementById("cart-items");
+
+let subTotal = 0;
+let row = ""; 
+
+for (let i = 0; i < cart.length; i++) {
+    let element = cart[i];
+    let total = element.price * element.quantity;
+    subTotal += total;
+
+    let row = `
+    <tr class="${i % 2 == 0 ? 'bg-red-100' : 'bg-white'} text-center">
+        <td class="py-2 px-2 border">${i + 1}</td>
+        <td class="py-2 px-2 border">
+            <div class="flex items-center justify-start gap-9">
+                <img src="${element.image}" alt="${element.titre}" class="w-8 h-8 ml-4">
+                <span class="font-medium">${element.titre}</span>
+            </div>
+        </td>
+        <td class="py-2 px-2 border">$${element.price}</td>
+        <td class="py-2 px-2 border">${element.quantity}</td>
+        <td class="py-2 px-2 border">$${total}</td>
+    </tr>
+    `;
+    tableBody.innerHTML += row;
+}
+
+
+const tax = subTotal * 0.20;
+const total = subTotal + tax;
+
+document.getElementById("subtotal").textContent = `$${subTotal}`;
+document.getElementById("tax").textContent = `$${tax}`;
+document.getElementById("total").textContent = `$${total}`;
+/*document.getElementById("confirm").addEventListener("click",function(){
+    
+})*/
